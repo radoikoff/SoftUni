e42bd2b1b900c96fe20330c86508de5acd107d55
radoikoff <radoikoff@gmail.com>
2017-01-26 21:39:26 +0200

    Arrays exercises 1 and 2

---
+++ Programming Fundamentals/Arrays/1. Largest Common End/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _1.Largest_Common_End
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputOne = Console.ReadLine().Split(' ').ToArray();
+            string[] inputTwo = Console.ReadLine().Split(' ').ToArray();
+           
+                        int hitCountLeft = HitCount(inputOne, inputTwo);
+            var InputOneReversed = inputOne.Reverse().ToArray();
+            var InputTwoReversed = inputTwo.Reverse().ToArray();
+            int hitCountRight = HitCount(InputOneReversed, InputTwoReversed);
+
+            Console.WriteLine(Math.Max(hitCountLeft, hitCountRight));
+        }
+
+        public static int HitCount (string[] input1, string[] input2)
+        {
+            int shortherLenght = Math.Min(input1.Length, input2.Length);
+            int hitCounter = 0;
+            for (int i = 0; i < shortherLenght; i++)
+            {
+                if (input1[i] == input2[i])
+                {
+                    hitCounter++;
+                }
+            }
+            return hitCounter;
+        }
+    }
+}
---
+++ Programming Fundamentals/Arrays/2. Rotate and Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _2.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var source = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int timesToRotate = int.Parse(Console.ReadLine());
+
+            int arrLength = source.Length;
+            var target = new int[arrLength];
+            var sum = new int[arrLength];
+
+            for (int rotate = 1; rotate <= timesToRotate; rotate++)
+            {
+
+                for (int i = 0; i < arrLength; i++)
+                {
+                    target[(i + 1) % arrLength] = source[i];
+                }
+
+                for (int i = 0; i < arrLength; i++)
+                {
+                    sum[i] += target[i];
+                }
+                //Console.WriteLine(string.Join(" ", target));
+
+                for (int i = 0; i < arrLength; i++)
+                {
+                    source[i] = target[i];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
