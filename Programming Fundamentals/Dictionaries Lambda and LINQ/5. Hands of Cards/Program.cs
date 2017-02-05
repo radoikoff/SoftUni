eaf3512f3a9461fc6809d572cde0acb9359304e9
radoikoff <radoikoff@gmail.com>
2017-02-06 01:04:25 +0200

    Added items 5,6,and 7 from dictionaries exercises

---
+++ Programming Fundamentals/Dictionaries Lambda and LINQ/5. Hands of Cards/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.Hands_of_Cards
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var cardsInfo = new Dictionary<string, List<string>>();
+            var cardPowers = GetCardPowers();
+            var cardTypes = GetCardTypes();
+            string input = Console.ReadLine();
+            while (input != "JOKER")
+            {
+
+                var item = input.Split(':');
+                var playerName = item[0];
+                var playerCards = item[1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries); //.Distinct();
+
+                foreach (var card in playerCards)
+                {
+                    if (!cardsInfo.ContainsKey(playerName))
+                    {
+                        cardsInfo.Add(playerName, new List<string>());
+                    }
+
+                    cardsInfo[playerName].AddRange(playerCards);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var player in cardsInfo)
+            {
+                var cards = player.Value.Distinct();
+                int points = 0;
+                foreach (var card in cards)
+                {
+                    var cardPower = card.Substring(0, card.Length - 1);
+                    var cardType = card.Substring(card.Length - 1);
+                    points += cardPowers[cardPower] * cardTypes[cardType];
+                }
+
+                Console.WriteLine($"{player.Key}: {points}");
+            }
+        }
+
+        public static Dictionary<string, int> GetCardPowers()
+        {
+            var power = new Dictionary<string, int>();
+            for (int i = 2; i <=10; i++)
+            {
+                power[i.ToString()] = i;
+            }
+            power["J"] = 11;
+            power["Q"] = 12;
+            power["K"] = 13;
+            power["A"] = 14;
+
+            return power;
+        }
+
+        public static Dictionary<string, int> GetCardTypes()
+        {
+            var type = new Dictionary<string, int>();
+            
+            type["S"] = 4;
+            type["H"] = 3;
+            type["D"] = 2;
+            type["C"] = 1;
+
+            return type;
+        }
+    }
+
+}
+
+
---
+++ Programming Fundamentals/Dictionaries Lambda and LINQ/7. Population Counter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.Population_Counter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var populationData = new Dictionary<string, Dictionary<string, long>>();
+            var line = Console.ReadLine();
+
+            while (line != "report")
+            {
+                var tokens = line.Split('|');
+                string city = tokens[0];
+                string country = tokens[1];
+                long cityPopulation = long.Parse(tokens[2]);
+
+                if (!populationData.ContainsKey(country))
+                {
+                    populationData[country] = new Dictionary<string, long>();
+                }
+                populationData[country].Add(city, cityPopulation);
+                line = Console.ReadLine();
+            }
+            foreach (var country in populationData.OrderByDescending(x => x.Value.Sum(y => y.Value)))
+            //foreach (var country in populationData.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                var cityCount = country.Value;
+                Console.WriteLine($"{country.Key} (total population: {country.Value.Values.Sum()})");
+                foreach (var city in cityCount.OrderByDescending(a => a.Value))
+                {
+                    Console.WriteLine($"=>{city.Key}: {city.Value}");
+                }
+            }
+        }
+    }
+}
