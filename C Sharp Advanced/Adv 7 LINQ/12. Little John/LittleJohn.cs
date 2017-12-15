f5a66697f255c370a9e01a58c01582d70ee0e131
radoikoff <radoikoff@gmail.com>
2017-12-15 10:51:51 +0200

    Added LINQ exercises

---
+++ C Sharp Advanced/Adv 7 LINQ/12. Little John/LittleJohn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _12.Little_John
+{
+    public class LittleJohn
+    {
+        public static void Main()
+        {
+            var regex = new Regex(@"(>>>-{5}>>)|(>>-{5}>)|(>-{5}>)");
+            int smallArrow = 0;
+            int mediumArrow = 0;
+            int bigArrow = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var input = Console.ReadLine();
+
+                foreach (Match match in regex.Matches(input))
+                {
+                    if (match.Groups[1].Success)
+                    {
+                        bigArrow++;
+                    }
+
+                    if (match.Groups[2].Success)
+                    {
+                        mediumArrow++;
+                    }
+
+                    if (match.Groups[3].Success)
+                    {
+                        smallArrow++;
+                    }
+
+                }
+            }
+            string numberinDec = string.Concat(smallArrow, mediumArrow, bigArrow);
+            string numberInBin = Convert.ToString(int.Parse(numberinDec), 2);
+            var reversed = string.Join(null, numberInBin.Reverse());
+            int result = Convert.ToInt32(numberInBin + reversed, 2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C Sharp Advanced/Adv 7 LINQ/StudentsByGroup/StudentsByGroup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsByGroup
+{
+    class StudentsByGroup
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, string>();
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var tokens = input.Split();
+                data.Add(tokens[0] + " " + tokens[1], tokens[2]);
+
+
+                input = Console.ReadLine();
+            }
+
+            var result = data.Where(d => d.Value.Equals("2")).OrderBy(x => x.Key).ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
