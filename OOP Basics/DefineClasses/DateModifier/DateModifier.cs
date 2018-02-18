4db88b51142854ee56782a253a2cc144a7ab4013
radoikoff <radoikoff@gmail.com>
2018-02-18 16:44:40 +0200

    Date Modifier exercise

---
+++ OOP Basics/DefineClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    class DateModifier
+    {
+        public static void Main()
+        {
+            var startDateAsString = Console.ReadLine();
+            var endDateAsString = Console.ReadLine();
+            var dateModifier = new DateModifierClass();
+            Console.WriteLine(dateModifier.GetDateDiff(startDateAsString, endDateAsString));
+        }
+    }
+}
---
+++ OOP Basics/DefineClasses/DateModifier/DateModifierClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class DateModifierClass
+    {
+
+        public int GetDateDiff(string startDate, string endDate)
+        {
+            string[] startDateSplit = startDate.Split();
+
+            int year = int.Parse(startDateSplit[0]);
+            int month = int.Parse(startDateSplit[1]);
+            int day = int.Parse(startDateSplit[2]);
+            var dateOne = new DateTime(year, month, day);
+
+            string[] endDateSplit = endDate.Split();
+            year = int.Parse(endDateSplit[0]);
+            month = int.Parse(endDateSplit[1]);
+            day = int.Parse(endDateSplit[2]);
+
+            var dateTwo = new DateTime(year, month, day);
+
+            return Math.Abs((dateTwo - dateOne).Days);
+        }
+
+    }
+}
