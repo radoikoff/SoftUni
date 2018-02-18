72adb1bbd8f44a7e2a1bd26790edb05be57ca3ec
radoikoff <radoikoff@gmail.com>
2018-02-18 15:29:22 +0200

    Constructors

--- OOP Basics/DefineClasses/Person.cs
+++ OOP Basics/DefineClasses/Person.cs
@@ -4,6 +4,26 @@ public class Person
     private string name;
     private int age;
 
+    public Person()
+    {
+        Name = "No Name";
+        Age = 1;
+    }
+
+    public Person(int age)
+    : this()
+    {
+        Age = age;
+    }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+
+
     public int Age
     {
         get { return age; }
--- OOP Basics/DefineClasses/Program.cs
+++ OOP Basics/DefineClasses/Program.cs
@@ -3,11 +3,12 @@ public class Program
 {
     public static void Main()
     {
-        var person = new Person();
-        person.Name = "Pesho";
-        person.Age = 20;
-        var person2 = new Person() { Name = "Gosho", Age = 18 };
-        var person3 = new Person() { Name = "Stamat", Age = 43 };
+        //var person = new Person();
+        //person.Name = "Pesho";
+        //person.Age = 20;
+        //var person2 = new Person() { Name = "Gosho", Age = 18 };
+        //var person3 = new Person() { Name = "Stamat", Age = 43 };
+        //var test = new Person(22);
 
     }
 }
