832901ade2e7149a29d99c0c005244fce4a605ff
radoikoff <radoikoff@gmail.com>
2018-02-18 14:56:27 +0200

    Person Class

---
+++ OOP Basics/DefineClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        var person = new Person();
+        person.Name = "Pesho";
+        person.Age = 20;
+        var person2 = new Person() { Name = "Gosho", Age = 18 };
+        var person3 = new Person() { Name = "Stamat", Age = 43 };
+
+    }
+}
+
