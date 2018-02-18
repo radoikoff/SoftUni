1603d9afda997bfccecb87e454ea2329297df714
radoikoff <radoikoff@gmail.com>
2018-02-18 18:20:24 +0200

    Speed Racing Exercise

---
+++ OOP Basics/DefineClasses/SpeedRacing/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        //model, fuel amount, fuel consumption for 1 kilometer and distance traveled
+
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKm;
+        private int distanceTravelled;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKm = fuelConsumptionPerKm;
+            this.distanceTravelled = 0;
+        }
+
+        public int DistanceTravelled
+        {
+            get { return distanceTravelled; }
+            set { distanceTravelled = value; }
+        }
+
+
+        public double FuelConsumptionPerKm
+        {
+            get { return fuelConsumptionPerKm; }
+            //set { fuelConsumptionPerKm = value; }
+        }
+
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+
+        public string Model
+        {
+            get { return model; }
+            //set { model = value; }
+        }
+
+        public bool CanCarMove(int distance)
+        {
+            if (distance * FuelConsumptionPerKm > FuelAmount)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:F2} {DistanceTravelled}";
+        }
+    }
+}
---
+++ OOP Basics/DefineClasses/SpeedRacing/SpeedRacing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class SpeedRacing
+    {
+        public static void Main()
+        {
+            var cars = new List<Car>();
+            var number = int.Parse(Console.ReadLine());
+            for (int counter = 0; counter < number; counter++)
+            {
+                var carData = Console.ReadLine().Split();
+                cars.Add(new Car(carData[0], double.Parse(carData[1]), double.Parse(carData[2])));
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var commandSplit = command.Split();
+                var carModel = commandSplit[1];
+                var distanceToTravel = int.Parse(commandSplit[2]);
+
+                var car = cars.FirstOrDefault(c => c.Model == carModel);
+                if (car.CanCarMove(distanceToTravel))
+                {
+                    car.DistanceTravelled += distanceToTravel;
+                    car.FuelAmount -= distanceToTravel * car.FuelConsumptionPerKm;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
