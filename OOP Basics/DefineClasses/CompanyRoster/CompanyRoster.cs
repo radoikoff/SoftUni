0cce2eaa76a0b8fe70a2bbe9b57228a5c3d5dea8
radoikoff <radoikoff@gmail.com>
2018-02-18 17:48:00 +0200

    Company Roster exercise

---
+++ OOP Basics/DefineClasses/CompanyRoster/CompanyRoster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyRoster
+{
+    public class CompanyRoster
+    {
+        public static void Main()
+        {
+            var employees = new List<Employee>();
+            var number = int.Parse(System.Console.ReadLine());
+            for (int counter = 0; counter < number; counter++)
+            {
+                var data = Console.ReadLine().Split();
+                var name = data[0];
+                var salary = Decimal.Parse(data[1]);
+                var position = data[2];
+                var department = data[3];
+
+                if (data.Length == 4)
+                {
+                    employees.Add(new Employee(name, salary, position, department));
+                }
+                else if (data.Length == 5)
+                {
+                    if (data[4].Contains("@"))
+                    {
+                        var email = data[4];
+                        employees.Add(new Employee(name, salary, position, department, email));
+                    }
+                    else
+                    {
+                        var age = int.Parse(data[4]);
+                        employees.Add(new Employee(name, salary, position, department, age));
+                    }
+                }
+                else if (data.Length == 6)
+                {
+                    var email = data[4];
+                    var age = int.Parse(data[5]);
+                    employees.Add(new Employee(name, salary, position, department, email, age));
+                }
+
+            }
+            var departmentName = employees.GroupBy(e => e.Department)
+                                    .Select(g => new { Department = g.Key, Salary = g.Average(e => e.Salary) })
+                                    .OrderByDescending(d => d.Salary)
+                                    .FirstOrDefault()
+                                    .Department;
+
+            Console.WriteLine($"Highest Average Salary: {departmentName}");
+            foreach (var employee in employees.Where(e => e.Department == departmentName).OrderByDescending(e => e.Salary))
+            {
+                Console.WriteLine(employee.ToString());
+            }
+
+
+        }
+    }
+}
---
+++ OOP Basics/DefineClasses/CompanyRoster/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyRoster
+{
+    public class Employee
+    {
+        private string name;
+        private decimal salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public Employee(string name, decimal salary, string position, string department)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.department = department;
+            this.age = -1;
+            this.email = "n/a";
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email, int age)
+            : this(name, salary, position, department)
+        {
+            this.Email = email;
+            this.Age = age;
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email)
+            : this(name, salary, position, department)
+        {
+            this.Email = email;
+            this.age = -1;
+        }
+
+        public Employee(string name, decimal salary, string position, string department, int age)
+            : this(name, salary, position, department)
+        {
+            this.Age = age;
+            this.email = "n/a";
+        }
+
+
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Salary:F2} {email} {age}";
+        }
+
+    }
+}
