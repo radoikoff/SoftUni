de2b3df2c48f20ae4cc41bddb5fde53c34e4c215
radoikoff <radoikoff@gmail.com>
2018-02-19 20:51:48 +0200

    Rectange Intersection Exercise

---
+++ OOP Basics/DefineClasses/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleIntersection
+{
+    public class Rectangle
+    {
+        private string id;
+        private double width;
+        private double height;
+        private double leftCoordinate;
+        private double topCoordinate;
+
+        public Rectangle(string id, double width, double height, double leftCoordinate, double topCoordinate)
+        {
+            this.id = id;
+            this.width = width;
+            this.height = height;
+            this.leftCoordinate = leftCoordinate;
+            this.topCoordinate = topCoordinate;
+        }
+
+        public double TopCoordinate
+        {
+            get { return topCoordinate; }
+            set { topCoordinate = value; }
+        }
+
+        public double LeftCoordinate
+        {
+            get { return leftCoordinate; }
+            set { leftCoordinate = value; }
+        }
+
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public bool IsIntersect(Rectangle rectangle)
+        {
+            bool isIntersect = false;
+
+            //A.X1 < B.X2: 
+            //A.X2 > B.X1: 
+            //A.Y1 < B.Y2: 
+            //A.Y2 > B.Y1: 
+
+            if (Math.Abs(this.leftCoordinate) < Math.Abs(rectangle.LeftCoordinate + rectangle.Width))
+            {
+                if (Math.Abs(this.leftCoordinate + this.width) >= Math.Abs(rectangle.LeftCoordinate))
+                {
+                    if (this.topCoordinate < Math.Abs((rectangle.TopCoordinate + rectangle.Height)))
+                    {
+                        if (Math.Abs(this.TopCoordinate + this.height) >= Math.Abs(rectangle.TopCoordinate))
+                        {
+                            isIntersect = true;
+                        }
+                    }
+                }
+            }
+
+            return isIntersect;
+        }
+
+    }
+}
---
+++ OOP Basics/DefineClasses/RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleIntersection
+{
+    public class RectangleIntersection
+    {
+        public static void Main()
+        {
+            var rectangles = new List<Rectangle>();
+            var input = Console.ReadLine().Split();
+            var numberOfRectangles = int.Parse(input[0]);
+            var numberOfChecks = int.Parse(input[1]);
+
+            for (int counter = 0; counter < numberOfRectangles; counter++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var id = tokens[0];
+                var width = double.Parse(tokens[1]);
+                var height = double.Parse(tokens[2]);
+                var leftCoordinate = double.Parse(tokens[3]);
+                var topCoordinate = double.Parse(tokens[4]);
+                rectangles.Add(new Rectangle(id, width, height, leftCoordinate, topCoordinate));
+            }
+
+            for (int counter = 0; counter < numberOfChecks; counter++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var rectangleOneId = tokens[0];
+                var rectangleTwoId = tokens[1];
+                var secondRectangle = rectangles.FirstOrDefault(r => r.Id.Equals(rectangleTwoId));
+                var firstRectangle = rectangles.FirstOrDefault(r => r.Id.Equals(rectangleOneId));
+
+                Console.WriteLine(firstRectangle.IsIntersect(secondRectangle).ToString().ToLower());
+            }
+
+        }
+    }
+}
