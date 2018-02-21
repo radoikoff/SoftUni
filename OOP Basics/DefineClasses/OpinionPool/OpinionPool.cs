bbe6df1b8a4f3cf9017f3165478b5ed6eafa62ac
radoikoff <radoikoff@gmail.com>
2018-02-21 22:10:22 +0200

    Opinion Poll Exercise

---
+++ OOP Basics/DefineClasses/OpinionPool/OpinionPool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class OpinionPool
+{
+    public static void Main()
+    {
+        var family = new List<Person>();
+        var number = int.Parse(System.Console.ReadLine());
+        for (int counter = 0; counter < number; counter++)
+        {
+            var data = Console.ReadLine().Split();
+            var name = data[0];
+            var age = int.Parse(data[1]);
+            family.Add(new Person(name, age));
+        }
+
+        foreach (var person in family.Where(p => p.Age > 30).OrderBy(p => p.Name))
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+
+
+    }
+}
+
---
+++ OOP Basics/DefineClasses/OpinionPool/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person()
+    {
+        Name = "No Name";
+        Age = 1;
+    }
+
+    public Person(int age)
+    : this()
+    {
+        Age = age;
+    }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {Age.ToString()}";
+    }
+
+}
+
