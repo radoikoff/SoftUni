cd78ff27e51587803f15caace94c198a3116c27e
radoikoff <radoikoff@gmail.com>
2018-02-21 21:32:39 +0200

    Family Tree Exercise

---
+++ OOP Basics/DefineClasses/FamilyTree/FamilyTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree
+{
+    public class FamilyTree
+    {
+        public static void Main()
+        {
+            var people = new List<Person>();
+            //var ties = new Dictionary<string, string>();
+            var ties = new Queue<string>();
+
+
+            var targetPersonData = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input.Contains(" - "))
+                {
+                    ties.Enqueue(input);
+                }
+                else
+                {
+                    //person mode
+                    var tokens = input.Split();
+                    var firstName = tokens[0].Trim();
+                    var lastName = tokens[1].Trim();
+                    var date = tokens[2].Trim();
+                    people.Add(new Person(firstName, lastName, date));
+                }
+            }
+
+            Person person = GetTargetPerson(targetPersonData, people);
+
+            //tie handling
+            while (ties.Count != 0)
+            {
+                var tie = ties.Dequeue();
+                var tokens = tie.Split('-');
+                var parentData = tokens[0].Trim();
+                var childData = tokens[1].Trim();
+
+                var parent = GetTargetPerson(parentData, people);
+                if (parent != null && parent.Equals(person))
+                {
+                    //search for match in parents
+                    var child = GetTargetPerson(childData, people);
+                    person.Children.Add(child);
+                }
+                else
+                {
+                    //search for match in childrens
+                    var child = GetTargetPerson(childData, people);
+                    if (child != null && child.Equals(person))
+                    {
+                        person.Parents.Add(parent);
+                    }
+                }
+            }
+
+            Console.WriteLine(person.ToString());
+        }
+
+        private static Person GetTargetPerson(string targetPersonData, List<Person> people)
+        {
+            if (targetPersonData.Contains($"/"))
+            {
+                //date
+                return people.FirstOrDefault(p => p.BirthDate == targetPersonData);
+            }
+            else
+            {
+                //names
+                var tokens = targetPersonData.Split();
+                return people.FirstOrDefault(p => p.FirstName == tokens[0] && p.LastName == tokens[1]);
+            }
+        }
+    }
+}
---
+++ OOP Basics/DefineClasses/FamilyTree/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree
+{
+    public class Person : IEquatable<Person>
+    {
+        public Person(string firstName, string lastName, string birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Parents = new List<Person>();
+            Children = new List<Person>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public List<Person> Parents { get; set; }
+        public List<Person> Children { get; set; }
+
+        public bool Equals(Person other)
+        { 
+            if (this.FirstName != other.FirstName) return false;
+            if (this.LastName != other.LastName) return false;
+            if (this.BirthDate != other.BirthDate) return false;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{FirstName} {LastName} {BirthDate}");
+            sb.AppendLine("Parents:");
+            foreach (var parent in Parents)
+            {
+                sb.AppendLine($"{parent.FirstName} {parent.LastName} {parent.BirthDate}");
+            }
+            sb.AppendLine("Children:");
+            foreach (var child in Children)
+            {
+                sb.AppendLine($"{child.FirstName} {child.LastName} {child.BirthDate}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
