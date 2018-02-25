8bdb8c9c9cd32a0b42eda93090053c81b8097fc9
radoikoff <radoikoff@gmail.com>
2018-02-25 22:59:36 +0200

    Encapsulation, ClassBox exersice

---
+++ OOP Basics/Encapsulation/ClassBox/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height)
+    {
+        this.Length = length;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double Length
+    {
+        get { return length; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Length cannot be zero or negative.");
+            }
+            length = value;
+        }
+    }
+
+    public double Width
+    {
+        get { return width; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Width cannot be zero or negative.");
+            }
+            width = value;
+        }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Height cannot be zero or negative.");
+            }
+            height = value;
+        }
+    }
+
+
+    public double SurfaceArea()
+    {
+        return 2 * length * width + 2 * length * height + 2 * width * height;
+    }
+
+    public double LateralSurfaceArea()
+    {
+        return 2 * length * height + 2 * width * height;
+    }
+
+    public double Volume()
+    {
+        return length * width * height;
+    }
+}
---
+++ OOP Basics/Encapsulation/ClassBox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var length = double.Parse(Console.ReadLine());
+        var width = double.Parse(Console.ReadLine());
+        var height = double.Parse(Console.ReadLine());
+
+        try
+        {
+            var box = new Box(length, width, height);
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.ParamName);
+        }
+
+
+
+    }
+}
+
+
