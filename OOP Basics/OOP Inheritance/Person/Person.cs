9579eb88096d268a2334b70dcdecb9f251d7e2f8
radoikoff <radoikoff@gmail.com>
2018-03-01 16:58:41 +0200

    Inheritance - Person exercises

---
+++ OOP Basics/OOP Inheritance/Person/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public virtual int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+
+            age = value;
+        }
+    }
+
+
+    protected string Name
+    {
+        get { return name; }
+        set
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            name = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+
+        return sb.ToString();
+    }
+
+
+}
