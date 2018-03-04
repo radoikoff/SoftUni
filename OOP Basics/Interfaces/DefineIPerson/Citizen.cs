c0ab93f68fd1ae6e6f142084e0b4f871f732a274
radoikoff <radoikoff@gmail.com>
2018-03-04 12:15:02 +0200

    Problem 1. Define an Interface IPerson

---
+++ OOP Basics/Interfaces/DefineIPerson/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Citizen : IPerson
+{
+    private string name;
+    private int age;
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public int Age
+    {
+        get { return age; }
+        private set { age = value; }
+    }
+
+
+    public string Name
+    {
+        get { return name; }
+        private set { name = value; }
+    }
+
+}
+
