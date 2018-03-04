441efa6192625fc2605c316e132d2be3f2b4114b
radoikoff <radoikoff@gmail.com>
2018-03-04 14:31:37 +0200

    Problem 4. Telephony

---
+++ OOP Basics/Interfaces/Telephony/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var phones = Console.ReadLine().Split().ToList();
+        var sites = Console.ReadLine().Split().ToList();
+        var phone = new Smartphone(phones, sites);
+
+        phone.Call();
+        phone.Browse();
+    }
+}
+
---
+++ OOP Basics/Interfaces/Telephony/Smartphone.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Smartphone : ISmartphone
+{
+    private List<string> phoneNumbers;
+    private List<string> sites;
+
+    public Smartphone(List<string> phoneNumbers, List<string> sites)
+    {
+        this.phoneNumbers = phoneNumbers;
+        this.sites = sites;
+    }
+
+    public void Call()
+    {
+        foreach (var number in phoneNumbers)
+        {
+            if (IsNumberValid(number))
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+
+    private bool IsNumberValid(string number)
+    {
+        if (number.All(c => c >= '0' && c <= '9'))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Browse()
+    {
+        foreach (var site in sites)
+        {
+            if (IsSiteValid(site))
+            {
+                Console.WriteLine($"Browsing: {site}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+
+        }
+    }
+
+    private bool IsSiteValid(string site)
+    {
+        if (site.Any(c => c >= '0' && c <= '9'))
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
