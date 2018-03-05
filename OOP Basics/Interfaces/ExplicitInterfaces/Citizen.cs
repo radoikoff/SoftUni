95ddf9a60177c805ab35d6f3c72f7fb3065360df
radoikoff <radoikoff@gmail.com>
2018-03-05 22:24:30 +0200

    Problem 10. Explicit Interfaces

---
+++ OOP Basics/Interfaces/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Citizen : IPerson, IResident
+{
+    public Citizen(string name, string country, int age)
+    {
+        Name = name;
+        Age = age;
+        Country = country;
+    }
+
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public string Country { get; private set; }
+
+    string IResident.GetName()
+    {
+        return "Mr/Ms/Mrs " + Name;
+    }
+    string IPerson.GetName()
+    {
+        return Name;
+    }
+}
+
---
+++ OOP Basics/Interfaces/ExplicitInterfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            var tokens = input.Split();
+            IPerson citizen = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+            Console.WriteLine(citizen.GetName());
+            IResident citizen2 = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+            Console.WriteLine(citizen2.GetName());
+        }
+    }
+}
+
