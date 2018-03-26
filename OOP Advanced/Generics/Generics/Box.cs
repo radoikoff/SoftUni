697ba09370e611367199832abb7e4b2352a4a0bf
radoikoff <radoikoff@gmail.com>
2018-03-26 18:04:14 +0300

    Problem 1. Generic Box of String

---
+++ OOP Advanced/Generics/Generics/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        public Box(T input)
+        {
+            this.Value = input;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            return Value.GetType().FullName;
+        }
+    }
+}
---
+++ OOP Advanced/Generics/Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+                var box = new Box<string>(input);
+
+                Console.WriteLine($"{box.ToString()}: {box.Value}");
+            }
+        }
+    }
+}
