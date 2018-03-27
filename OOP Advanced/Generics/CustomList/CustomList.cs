a109fb33735b97f9dd67d1431570e1d06920510f
radoikoff <radoikoff@gmail.com>
2018-03-27 15:06:58 +0300

    Problem 7. Custom List

---
+++ OOP Advanced/Generics/CustomList/CustomList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomList
+{
+    public class CustomList<T> where T : IComparable
+    {
+        private readonly List<T> items;
+
+        public IReadOnlyCollection<T> Items => this.items.AsReadOnly();
+
+        public CustomList()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.items.Add(element);
+        }
+
+        public T Remove(int index)
+        {
+            T item = this.items[index];
+            this.items.RemoveAt(index);
+            return item;
+        }
+
+        public bool Contains(T element)
+        {
+            return this.items.Contains(element);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var itemAtFirstPosition = this.items[firstIndex];
+            this.items[firstIndex] = this.items[secondIndex];
+            this.items[secondIndex] = itemAtFirstPosition;
+        }
+
+        public int CountGreaterThan(T element)
+        {
+            return this.items.Count(t => t.CompareTo(element) > 0);
+        }
+
+        public T Max()
+        {
+            return this.items.Max();
+        }
+
+        public T Min()
+        {
+            return this.items.Min();
+        }
+    }
+}
---
+++ OOP Advanced/Generics/CustomList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomList<string> data = new CustomList<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var args = input.Split();
+                string command = args[0];
+                string element = "";
+                //string result = string.Empty;
+
+                switch (command)
+                {
+                    case "Add":
+                        element = args[1];
+                        data.Add(element);
+                        break;
+                    case "Remove":
+                        element = args[1];
+                        data.Remove(int.Parse(element));
+                        break;
+                    case "Contains":
+                        element = args[1];
+                        Console.WriteLine(data.Contains(element).ToString());
+                        break;
+                    case "Swap":
+                        string elementOne = args[1];
+                        string elementTwo = args[2];
+                        data.Swap(int.Parse(elementOne), int.Parse(elementTwo));
+                        break;
+                    case "Greater":
+                        element = args[1];
+                        Console.WriteLine(data.CountGreaterThan(element).ToString());
+                        break;
+                    case "Max":
+                        Console.WriteLine(data.Max().ToString());
+                        break;
+                    case "Min":
+                        Console.WriteLine(data.Min().ToString());
+                        break;
+                    case "Print":
+                        foreach (var item in data.Items)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
