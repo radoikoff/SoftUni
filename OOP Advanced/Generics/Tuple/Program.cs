feef0ea304806f19f3987c02178e4de5378f38da
radoikoff <radoikoff@gmail.com>
2018-03-27 23:26:10 +0300

    Problem 10.  Tuple

---
+++ OOP Advanced/Generics/Tuple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            var args = Console.ReadLine().Split();
+            var fullName = args[0] + ' ' + args[1];
+            var firstTuple = new Tuple<string, string>(fullName, args[2]);
+            Console.WriteLine($"{firstTuple.FirstItem} -> {firstTuple.SecondItem}");
+
+            args = Console.ReadLine().Split();
+            var secondTuple = new Tuple<string, int>(args[0], int.Parse(args[1]));
+            Console.WriteLine($"{secondTuple.FirstItem} -> {secondTuple.SecondItem}");
+
+            args = Console.ReadLine().Split();
+            var thirdTuple = new Tuple<int, double>(int.Parse(args[0]), double.Parse(args[1]));
+            Console.WriteLine($"{thirdTuple.FirstItem} -> {thirdTuple.SecondItem}");
+
+        }
+
+    }
+}
---
+++ OOP Advanced/Generics/Tuple/Tuple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 FirstItem { get; private set; }
+        public T2 SecondItem { get; private set; }
+
+        public Tuple(T1 firstItem, T2 secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+    }
+}
