dc16cfa1b1ca3539bd319dc8818e2df28a29c610
radoikoff <radoikoff@gmail.com>
2018-03-28 09:22:10 +0300

    Problem 11.  Threeuple

--- OOP Advanced/Generics/Tuple/Program.cs
+++ OOP Advanced/Generics/Tuple/Program.cs
@@ -8,17 +8,19 @@ namespace Tuple
         public static void Main()
         {
             var args = Console.ReadLine().Split();
-            var fullName = args[0] + ' ' + args[1];
-            var firstTuple = new Tuple<string, string>(fullName, args[2]);
-            Console.WriteLine($"{firstTuple.FirstItem} -> {firstTuple.SecondItem}");
+            var fullName = args[0] + " " + args[1];
+            var firstTuple = new Threeuple<string, string, string>(fullName, args[2], args[3]);
+            Console.WriteLine(firstTuple.ToString());
 
             args = Console.ReadLine().Split();
-            var secondTuple = new Tuple<string, int>(args[0], int.Parse(args[1]));
-            Console.WriteLine($"{secondTuple.FirstItem} -> {secondTuple.SecondItem}");
+            bool isDrunk = args[2] == "drunk";
+
+            var secondTuple = new Threeuple<string, int, bool>(args[0], int.Parse(args[1]), isDrunk);
+            Console.WriteLine(secondTuple.ToString());
 
             args = Console.ReadLine().Split();
-            var thirdTuple = new Tuple<int, double>(int.Parse(args[0]), double.Parse(args[1]));
-            Console.WriteLine($"{thirdTuple.FirstItem} -> {thirdTuple.SecondItem}");
+            var thirdTuple = new Threeuple<string, double, string>(args[0], double.Parse(args[1]), args[2]);
+            Console.WriteLine(thirdTuple.ToString());
 
         }
 
---
+++ OOP Advanced/Generics/Tuple/Threeuple.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public T1 FirstItem { get; set; }
+        public T2 SecondItem { get; set; }
+        public T3 ThirdItem { get; set; }
+
+        public Threeuple()
+        {
+
+        }
+
+        public Threeuple(T1 firstItem, T2 secondItem, T3 thirdItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+            this.ThirdItem = thirdItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThirdItem}";
+        }
+    }
+}
