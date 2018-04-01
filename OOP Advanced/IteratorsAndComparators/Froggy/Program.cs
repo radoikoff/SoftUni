cf1e0dfdd98b9fbb66165c7ebd8daf53500697a0
radoikoff <radoikoff@gmail.com>
2018-04-01 21:26:56 +0300

    Problem 4. Froggy

---
+++ OOP Advanced/IteratorsAndComparators/Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] stones = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
