eed933c41f9b94e4c540be596384acc848c7ca7d
radoikoff <radoikoff@gmail.com>
2018-04-01 21:56:04 +0300

    Problem 7. Equality Logic

---
+++ OOP Advanced/IteratorsAndComparators/EqualityLogic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sorted = new SortedSet<Person>();
+            var hash = new HashSet<Person>();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] args = Console.ReadLine().Split();
+                string name = args[0];
+                int age = int.Parse(args[1]);
+
+                Person person = new Person(name, age);
+                sorted.Add(person);
+                hash.Add(person);
+            }
+
+            Console.WriteLine(sorted.Count);
+            Console.WriteLine(hash.Count);
+        }
+    }
+}
