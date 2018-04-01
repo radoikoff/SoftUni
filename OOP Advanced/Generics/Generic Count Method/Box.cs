f89aa5b9a304a9cc9c26fc5bde930debfe4e6137
radoikoff <radoikoff@gmail.com>
2018-04-01 19:36:54 +0300

    Generic Count Method

---
+++ OOP Advanced/Generics/Generic Count Method/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCountMethod
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box()
+        {
+            this.Elements = new List<T>();
+        }
+
+        public Box(T input)
+           : base()
+        {
+            this.Value = input;
+
+        }
+
+        public T Value { get; private set; }
+
+        public List<T> Elements { get; set; }
+
+
+        public int EqualsCount(T element)
+        {
+            return this.Elements.Count(t => t.CompareTo(element) > 0);
+        }
+
+        public override string ToString()
+        {
+            return Value.GetType().FullName;
+        }
+    }
+}
---
+++ OOP Advanced/Generics/Generic Count Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCountMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<double>();
+
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                box.Elements.Add(input);
+            }
+
+            var compareToValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.EqualsCount(compareToValue));
+        }
+
+    }
+}
