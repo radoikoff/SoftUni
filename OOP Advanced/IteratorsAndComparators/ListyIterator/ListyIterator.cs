94c5257c00414c48030473801330335dbd6b9f9d
radoikoff <radoikoff@gmail.com>
2018-04-02 21:07:39 +0300

    Problem 1. ListyIterator

---
+++ OOP Advanced/IteratorsAndComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        public ListyIterator(IEnumerable<T> collection)
+        {
+            this.collection = new List<T>(collection);
+            currIndex = 0;
+        }
+
+        private List<T> collection;
+        private int currIndex;
+
+        public bool Move()
+        {
+            if (!this.HasNext()) return false;
+            this.currIndex++;
+            return true;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidOperationException($"Invalid Operation!");
+            }
+            Console.WriteLine(collection[currIndex]);
+        }
+
+        public bool HasNext()
+        {
+            return currIndex < collection.Count - 1;
+        }
+    }
+}
---
+++ OOP Advanced/IteratorsAndComparators/ListyIterator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            var command = Console.ReadLine();
+            var data = command.Split().Skip(1);
+
+            var listyIterator = new ListyIterator<string>(data);
+
+            while (!(command = Console.ReadLine()).Equals("END"))
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listyIterator.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                }
+            }
+        }
+
+    }
+}
