fcfb3aab255c80519dd61b2a1f195833b8e7799c
radoikoff <radoikoff@gmail.com>
2018-04-02 21:37:41 +0300

    Problem 3. Stack

---
+++ OOP Advanced/IteratorsAndComparators/Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var cmdargs = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string command = cmdargs[0];
+            var stack = new Stack<int>();
+
+            while (command != "END")
+            {
+                if (command == "Push")
+                {
+                    var args = cmdargs.Skip(1).ToList();
+                    for (int i = 0; i < args.Count; i++)
+                    {
+                        stack.Push(int.Parse(args[i]));
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return;
+                    }
+                }
+
+                cmdargs = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                command = cmdargs[0];
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+
+        }
+    }
+}
