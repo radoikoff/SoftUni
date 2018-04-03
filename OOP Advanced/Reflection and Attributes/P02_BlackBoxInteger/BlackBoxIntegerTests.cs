38e6fdc8d5633c6868701022ef59cf0b58e1c32a
radoikoff <radoikoff@gmail.com>
2018-04-03 22:15:28 +0300

    Problem 2. Black Box Integer

---
+++ OOP Advanced/Reflection and Attributes/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            var type = typeof(BlackBoxInteger);
+            FieldInfo innerValue = type.GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var instance = Activator.CreateInstance(type, true);
+
+            string command = "";
+            while ((command = Console.ReadLine()) !="END")
+            {
+                var args = command.Split('_');
+
+                var methodName = args[0];
+                int value = int.Parse(args[1]);
+
+                MethodInfo method = methods.FirstOrDefault(m => m.Name == methodName);
+
+                method.Invoke(instance, new object[] { value });
+
+                Console.WriteLine(innerValue.GetValue(instance));
+            }
+        }
+    }
+}
