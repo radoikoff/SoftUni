2ca7ff309d804f36d0f4dc7ae0f313470a107039
radoikoff <radoikoff@gmail.com>
2018-06-30 17:56:01 +0300

    DB Adv ADO.NET 2.Villain Names

--- DB Advanced/AdoNET/01_InitialSetup/Configuration.cs
+++ DB Advanced/AdoNET/01_InitialSetup/Configuration.cs
@@ -6,6 +6,6 @@ namespace _01_InitialSetup
 {
     public class Configuration
     {
-        public const string ConnectionString = @"Server=(local)\SQLEXPRESS; Integrated Security=True;";  
+        public const string ConnectionString = @"Server=(local)\SQLEXPRESS; Database=MinionsDB; Integrated Security=True;";  
     }
 }
---
+++ DB Advanced/AdoNET/02VillainNames/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _02VillainNames
+{
+    using _01_InitialSetup;
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string villanInfo = "SELECT v.Name AS VillanName, COUNT(MinionId) AS MinionCount FROM Villains AS v JOIN MinionsVillains AS mv ON mv.VillainId = v.Id GROUP BY v.Name HAVING COUNT(MinionId) >= 3 ORDER BY COUNT(MinionId) DESC";
+
+                using (SqlCommand cmd = new SqlCommand(villanInfo, connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]} - {reader[1]}");
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
