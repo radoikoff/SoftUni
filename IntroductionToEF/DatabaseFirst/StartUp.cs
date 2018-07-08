4c56c9ff28cfb863aec6edacac6823e7938f12eb
radoikoff <radoikoff@gmail.com>
2018-07-08 22:38:06 +0300

    11. Find Latest 10 Projects

--- IntroductionToEF/DatabaseFirst/StartUp.cs
+++ IntroductionToEF/DatabaseFirst/StartUp.cs
@@ -13,18 +13,22 @@ namespace P02_DatabaseFirst
         {
             using (var context = new SoftUniContext())
             {
-                var employee147 = context.Employees.Find(147);
                 var projects = context.Projects
-                                      .Where(p => p.EmployeesProjects.Any(ep => ep.EmployeeId == 147))
-                                      .OrderBy(p=>p.Name);
+                                      .OrderByDescending(p => p.StartDate)
+                                      .Take(10)
+                                      .OrderBy(p => p.Name)
+                                      .ToArray();
 
 
                 using (StreamWriter sw = new StreamWriter("../Result.txt"))
                 {
-                    sw.WriteLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
                     foreach (var e in projects)
                     {
                         sw.WriteLine(e.Name);
+                    
+                        sw.WriteLine(e.Description);
+
+                        sw.WriteLine(e.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
                     }
 
                 }
