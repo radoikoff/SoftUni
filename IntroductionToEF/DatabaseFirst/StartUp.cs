2d0a349c741b9d81b4666a76f563954c63bc6455
radoikoff <radoikoff@gmail.com>
2018-07-08 18:53:32 +0300

    3. Employees Full Information

---
+++ IntroductionToEF/DatabaseFirst/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using P02_DatabaseFirst.Data;
+using System.Linq;
+using System;
+using System.IO;
+
+namespace P02_DatabaseFirst
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var emploees = context.Employees.OrderBy(e => e.EmployeeId).ToList();
+
+                using (StreamWriter sw = new StreamWriter("../Result.txt"))
+                {
+                    foreach (var e in emploees)
+                    {
+                        sw.WriteLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}");
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
