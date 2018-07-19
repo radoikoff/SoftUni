544ca05b989f8949006753cf0a22ef9cfe560181
radoikoff <radoikoff@gmail.com>
2018-07-19 20:49:45 +0300

    Advanced Querying 1.Age Restriction

--- DB Advanced/Advanced Querying/BookShop/StartUp.cs
+++ DB Advanced/Advanced Querying/BookShop/StartUp.cs
@@ -2,6 +2,9 @@
 {
     using BookShop.Data;
     using BookShop.Initializer;
+    using BookShop.Models;
+    using System;
+    using System.Linq;
 
     public class StartUp
     {
@@ -9,8 +12,28 @@
         {
             using (var db = new BookShopContext())
             {
-                DbInitializer.ResetDatabase(db);
+                //DbInitializer.ResetDatabase(db);
+                string command = Console.ReadLine();
+                string result = GetBooksByAgeRestriction(db, command);
+                Console.WriteLine(result);
+
             }
+
+
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
+
+            var books = context.Books
+                               .Where(b => b.AgeRestriction == ageRestriction)
+                               .OrderBy(b => b.Title)
+                               .Select(b => b.Title)
+                               .ToArray();
+
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
