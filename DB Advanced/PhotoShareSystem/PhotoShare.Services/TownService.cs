dcc9356c1b07c07bca98fce4de3747733217f868
radoikoff <radoikoff@gmail.com>
2018-07-26 17:54:59 +0300

    PhotoShare - Services implemented

--- DB Advanced/PhotoShareSystem/PhotoShare.Services/TownService.cs
+++ DB Advanced/PhotoShareSystem/PhotoShare.Services/TownService.cs
@@ -1,7 +1,54 @@
 ﻿namespace PhotoShare.Services
 {
-    public class TownService
+    using AutoMapper.QueryableExtensions;
+    using PhotoShare.Data;
+    using PhotoShare.Models;
+    using PhotoShare.Services.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TownService : ITownService
     {
-       
+        private readonly PhotoShareContext context;
+
+        public TownService(PhotoShareContext context)
+        {
+            this.context = context;
+        }
+
+
+        public TModel ById<TModel>(int id)
+            => By<TModel>(t => t.Id == id).SingleOrDefault();
+
+        public TModel ByName<TModel>(string name)
+            => By<TModel>(t => t.Name == name).SingleOrDefault();
+
+        public bool Exists(int id)
+            => ById<Town>(id) != null;
+
+        public bool Exists(string name)
+            => ByName<Town>(name) != null;
+
+
+        public Town Add(string townName, string countryName)
+        {
+            var town = new Town
+            {
+                Name = townName,
+                Country = countryName
+            };
+
+            this.context.Towns.Add(town);
+            this.context.SaveChanges();
+
+            return town;
+        }
+
+        private IEnumerable<TModel> By<TModel>(Func<Town, bool> predicate)
+            => this.context.Towns
+                           .Where(predicate)
+                           .AsQueryable()
+                           .ProjectTo<TModel>();
     }
 }
