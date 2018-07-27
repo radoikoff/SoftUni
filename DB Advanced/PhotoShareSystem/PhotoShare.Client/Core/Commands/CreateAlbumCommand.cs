92cc48819faa3f4aca0a5b4e87b30acadbe03d59
radoikoff <radoikoff@gmail.com>
2018-07-27 14:00:50 +0300

    PhotoShare - Create Album command

--- DB Advanced/PhotoShareSystem/PhotoShare.Client/Core/Commands/CreateAlbumCommand.cs
+++ DB Advanced/PhotoShareSystem/PhotoShare.Client/Core/Commands/CreateAlbumCommand.cs
@@ -1,8 +1,11 @@
 ﻿namespace PhotoShare.Client.Core.Commands
 {
     using System;
-
+    using System.Linq;
     using Contracts;
+    using PhotoShare.Client.Core.Dtos;
+    using PhotoShare.Client.Utilities;
+    using PhotoShare.Models.Enums;
     using Services.Contracts;
 
 
@@ -22,7 +25,50 @@
         // CreateAlbum <username> <albumTitle> <BgColor> <tag1> <tag2>...<tagN>
         public string Execute(string[] data)
         {
-            throw new NotImplementedException();
+            string username = data[0];
+            string albumTitle = data[1];
+            string color = data[2];
+            string[] tags = data.Skip(3).ToArray();
+
+            bool userExsits = this.userService.Exists(username);
+
+            if (!userExsits)
+            {
+                throw new ArgumentException($"User {username} not found!");
+            }
+
+            var userId = this.userService.ByUsername<UserDto>(username).Id;
+
+            bool albumExists = this.albumService.Exists(albumTitle);
+
+            if (albumExists)
+            {
+                throw new ArgumentException($"Album {albumTitle} exists!");
+            }
+
+            var isValidColor = Enum.TryParse<Color>(color, out Color result);
+
+            if (!isValidColor)
+            {
+                throw new ArgumentException($"Color {color} not found!");
+            }
+
+            for (int i = 0; i < tags.Length; i++)
+            {
+                tags[i] = tags[i].ValidateOrTransform();
+
+                bool currentTagExists = this.tagService.Exists(tags[i]);
+
+                if (!currentTagExists)
+                {
+                    throw new ArgumentException($"Invalid tags!");
+                }
+            }
+
+
+            this.albumService.Create(userId, albumTitle, color, tags);
+
+            return $"Album {albumTitle} successfully created!";
         }
     }
 }
